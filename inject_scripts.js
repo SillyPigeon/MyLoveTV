55be850674a1d99158dbbd10a158bed32d0b1b4f
huangzhenhang-HOMEPC2021 <biechusheng233@gmail.com>
2022-03-02 21:19:30 +0800

    Add dashJS to player

--- inject_scripts.js
+++ inject_scripts.js
@@ -1,10 +1,31 @@
+//akin code
+function injectJs(tabId, fileName, succeededMessage, addedToHead, addedAsFirstChild, withOption) {
+    var pluginPath = fileName.indexOf('cdn.') !== -1 ? fileName : chrome.extension.getURL(fileName);
+    var checkAlreadyInjected = 'if (d.head.getElementsByTagName("script").length>0 && [].slice.call(d.head.getElementsByTagName("script")).' +
+    'map(function(l) { return l.src.indexOf("' + fileName + '")!==-1; }).reduce(function(a,b) { return a || b })==true) return;';
+    var injectedScript = '(function(d){' + checkAlreadyInjected + 'var e=d.createElement("script");' +
+    (withOption ? 'e.setAttribute("' + withOption + '", "' + withOption + '");' : '') +
+    'e.setAttribute("type","text/javascript");e.setAttribute("src","' + pluginPath + '");' +
+    (addedToHead ? 'd.head' : 'd.body') +
+    (addedAsFirstChild ? '.insertBefore(e,d.head.firstChild)' : '.appendChild(e)') +
+    '}(document));';
+    //add script to DOM
+    var newScript = document.createElement("script");
+    var inlineScript = document.createTextNode(injectedScript);
+    newScript.appendChild(inlineScript);
+    document.getElementsByTagName("head")[0].appendChild(newScript);
+}
+
+
 function setupAndLoad() {
 
     // inject hbbtv polyfill
     var s = document.createElement('script');
-    s.src = chrome.runtime.getURL('hbbtv_polyfill/hbbtv_polyfill.js');
+    s.src = chrome.runtime.getURL('hbbtv_polyfill/index.js');
     s.type = "module";
-    // after polyfill has been loaded, load the rest of the hbbtv plugin.
     (document.head || document.documentElement).appendChild(s);
+
+    // inject dashJS
+    injectJs(document.location.tabId, 'https://cdn.dashjs.org/latest/dash.all.min.js', 'DASH.js injection done.', true, false, 'async');
 }
 setupAndLoad()
